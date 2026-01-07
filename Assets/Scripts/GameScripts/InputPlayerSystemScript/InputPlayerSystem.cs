a95a7825c73967ce90f0f8757ef305a97727c7e4
Danil Bogodvid <d.bogodvid@kefirgames.com>
2026-01-07 16:33:50 +0300

    fix: inputplayersystem

--- Assets/Scripts/GameScripts/InputPlayerSystemScript/InputPlayerSystem.cs
+++ Assets/Scripts/GameScripts/InputPlayerSystemScript/InputPlayerSystem.cs
@@ -2,12 +2,15 @@ namespace GameScripts.InputPlayerSystemScript
 {
     public class InputPlayerSystem : IGameSystem
     {
+        public InputPlayerModel Model;
+        
         public void InitSystem(GameSystemsHandler context)
         {
         }
 
         public void UpdateSystem(float deltaTime, GameSystemsHandler context)
         {
+            Model.UpdateModel(deltaTime, context);
         }
     }
 }

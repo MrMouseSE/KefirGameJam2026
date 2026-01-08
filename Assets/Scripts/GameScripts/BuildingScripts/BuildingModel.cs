f90e3a17c0cff0a4cb71a9af4d067882c1e9ba92
Maksatus <streetworkout042000@gmail.com>
2026-01-09 00:28:24 +0300

    fix: merge

--- Assets/Scripts/GameScripts/BuildingScripts/BuildingModel.cs
+++ Assets/Scripts/GameScripts/BuildingScripts/BuildingModel.cs
@@ -1,8 +1,6 @@
-using System;
 using System.Collections.Generic;
 using System.Linq;
 using GameScripts.BugsScripts;
-using Unity.VisualScripting;
 using UnityEngine;
 using Object = UnityEngine.Object;
 
